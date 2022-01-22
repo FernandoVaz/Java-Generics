38453a50b2784f5ab5de38105c83dcc8c56e6319
Fernando Vaz <fndmvaz@gmail.com>
2022-01-22 17:25:21 -0300

    Changing the structure for lessons

--- Generics.java
+++ Generics.java
@@ -1,14 +1,29 @@
 import java.util.ArrayList;
 
 class Generics {
-	
+
 	public static void main(String[] args) {
 
-		// ArrayList of objects, in order to access the Strings added
-		// we must cast the names.get() into a String.
-		// This is problematic since we can get type error at runtime
-		// Generics helps errors happen during compilation time.
+		switch(args[0]) {
+			case "1":
+				genericsExample1();
+				break;
+			case "2":
+				genericsExample2();
+				break;
+			default:
+				break;
+		}
+	}
 
+
+
+	// ArrayList of objects, in order to access the Strings added
+	// we must cast the names.get() into a String.
+	// This is problematic since we can get type error at runtime
+	// Generics helps errors happen during compilation time.
+	public static void genericsExample1() {
+		
 		ArrayList names = new ArrayList();
 
 		names.add("NameOne");
@@ -16,5 +31,12 @@ class Generics {
 		names.add("Name2");
 
 		String myName = (String) names.get(0);
+		System.out.println(myName);
+	}
+
+
+	// Unboxing and Autoboxing, the concepts of Wrapping primitives
+	public static void genericsExample2() {
+
 	}
 }
