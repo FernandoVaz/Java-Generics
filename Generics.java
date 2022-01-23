9da242853ca41983785d49f456f9f7fd830a66f8
Fernando Vaz <fndmvaz@gmail.com>
2022-01-23 07:36:11 -0300

    Autoboxing and unboxing in the context of generics

--- Generics.java
+++ Generics.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Generics {
 
@@ -11,17 +13,14 @@ class Generics {
 			case "2":
 				genericsExample2();
 				break;
+			case "3":
+				genericsExample3();
 			default:
 				break;
 		}
 	}
 
 
-
-	// ArrayList of objects, in order to access the Strings added
-	// we must cast the names.get() into a String.
-	// This is problematic since we can get type error at runtime
-	// Generics helps errors happen during compilation time.
 	public static void genericsExample1() {
 		
 		ArrayList names = new ArrayList();
@@ -30,13 +29,50 @@ class Generics {
 		names.add("Name1");
 		names.add("Name2");
 
+
+		System.out.println("\n############# Program description:\n");
+		System.out.println("ArrayList of objects, in order to access the Strings added \nwe must cast the names.get() into a String. \nThis is problematic since we can get type error at runtime \nGenerics helps errors happen during compilation time.");
+		System.out.println("\n############# Program outputs:\n");
 		String myName = (String) names.get(0);
 		System.out.println(myName);
 	}
 
-
-	// Unboxing and Autoboxing, the concepts of Wrapping primitives
 	public static void genericsExample2() {
+
+		System.out.println("\n############# Program description:\n");
+		System.out.println("Unboxing and Autoboxing, the concepts of Wrapping primitives");
+		System.out.println("Wrapper Objects is a way to treat an primitive as an Object");
+		System.out.println("Every primitive has an wrapper classes");
+		System.out.println("\n############# Program outputs:\n");
+
+		// Autoboxing
+		int z = 10;
+		Integer a = z;
+
+		// Unboxing
+		Integer x = new Integer(10);
+		int i = x;
+
+		List<Integer> list = Arrays.asList(10, 20, 30);
+
+	}
+
+	public static void genericsExample3() {
+
+		System.out.println("\n############# Program description:\n");
+		System.out.println("Parametized types");
+		System.out.println("List<E> is a generic type that takes a E as a type parameter");
+		System.out.println("\n############# Program outputs:\n");
+
+		// Autoboxing
+		int z = 10;
+		Integer a = z;
+
+		// Unboxing
+		Integer x = new Integer(10);
+		int i = x;
+
+		List<Integer> list = Arrays.asList(10, 20, 30);
 
 	}
 }
